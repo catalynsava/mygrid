72cdf7dd02083cda4bc682c8c6d6eb67f466e467
catalynsava <catalynsava@gmail.com>
2026-06-08 09:50:20 +0300

    commit and push

--- query.sql
+++ query.sql
@@ -8,7 +8,8 @@ SELECT
 	, capitol_2a.alta_localitate
 	, capitol_2a.total
  FROM capitol_2a 
-   INNER JOIN adrese_roluri
-    ON capitol_2a.id = adrese_roluri.id AND adrese_roluri.id = '0002732d-2dce-432b-b22a-62c89fbda14b'
- RIGHT JOIN cfg_capitol_2a
-	ON capitol_2a.cod = cfg_capitol_2a.cod;
+	INNER JOIN adrese_roluri
+    	ON capitol_2a.id = adrese_roluri.id 
+		AND adrese_roluri.id = '0002732d-2dce-432b-b22a-62c89fbda14b'
+	RIGHT JOIN cfg_capitol_2a
+		ON capitol_2a.cod = cfg_capitol_2a.cod;
